e4a845fc9a3e934dce47e381d740b5da7901adc5
wctjerry <wctjerry@>
2016-06-06 10:02:37 +0800

    Added decrement function and randomly background color change

--- p2/Click Counter/Click Counter/ViewController.swift
+++ p2/Click Counter/Click Counter/ViewController.swift
@@ -27,22 +27,59 @@ class ViewController: UIViewController {
         self.label = label
         
         
-        // Button
-        var button = UIButton()
-        button.frame = CGRectMake(150, 250, 60, 60)
-        button.setTitle("Click", forState: .Normal)
-        button.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        // increment Button
+        var incrementButton = UIButton()
+        incrementButton.frame = CGRectMake(150, 250, 120, 60)
+        incrementButton.setTitle("Increase", forState: .Normal)
+        incrementButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
         
-        self.view.addSubview(button)
-        button.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(incrementButton)
+        incrementButton.addTarget(self, action: "incrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        incrementButton.addTarget(self, action: "changeBackgroundColor", forControlEvents: UIControlEvents.TouchUpInside)
         
+        
+        // decrement Button
+        var decrementButton = UIButton()
+        decrementButton.frame = CGRectMake(150, 350, 120, 60)
+        decrementButton.setTitle("Decrease", forState: .Normal)
+        decrementButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+        
+        self.view.addSubview(decrementButton)
+        decrementButton.addTarget(self, action: "decrementCount", forControlEvents: UIControlEvents.TouchUpInside)
+        decrementButton.addTarget(self, action: "changeBackgroundColor", forControlEvents: UIControlEvents.TouchUpInside)
     }
     
     func incrementCount() {
         self.count += 1
         self.label?.text = "\(self.count)"
     }
+    
+    func decrementCount() {
+        self.count -= 1
+        self.label?.text = "\(self.count)"
+    }
+    
+    func changeBackgroundColor() {
+        self.view.backgroundColor = UIColor.randomColor()
+    }
+}
 
+// Add random background color function: http://stackoverflow.com/questions/29779128/how-to-make-a-random-background-color-with-swift
 
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    static func randomColor() -> UIColor {
+        // If you wanted a random alpha, just create another
+        // random number for that too.
+        return UIColor(red:   .random(),
+                       green: .random(),
+                       blue:  .random(),
+                       alpha: 1.0)
+    }
 }
 
